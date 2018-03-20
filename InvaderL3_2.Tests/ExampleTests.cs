8e93e97399efe3867d30ddab9a9099bf63128c9b
boleaczek <bart.oleaczek@gmail.com>
2018-03-20 16:46:52 +0100

    added tests

---
+++ InvaderL3_2.Tests/ExampleTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvaderL3_2.Tests
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
