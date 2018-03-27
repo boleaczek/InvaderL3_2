cdddf272439d58c90597b3c93468defac4868b29
boleaczek <bart.oleaczek@gmail.com>
2018-03-27 16:41:40 +0200

    implementacja interfejsow w interfejsach

--- InvaderLogicLibrary/IEnemy.cs
+++ InvaderLogicLibrary/IEnemy.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InvaderLogicLibrary
 {
-    interface IEnemy
+    interface IEnemy : IEntity, IObserver, IEnemyFlyweight
     {
     }
 }
--- InvaderLogicLibrary/IPlayer.cs
+++ InvaderLogicLibrary/IPlayer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InvaderLogicLibrary
 {
-    interface IPlayer
+    interface IPlayer : IEntity, IObserver
     {
         void UpdatePosition();
     }
