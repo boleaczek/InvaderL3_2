309241c1d3b7cdfb1b7a54a63cdc54a4efa6cc0a
Kamil R <kamil.r@interia.pl>
2018-03-27 16:21:06 +0200

    interfejsy kamilowe

---
+++ InvaderLogicLibrary/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvaderLogicLibrary
+{
+    interface IEntity
+    {
+        IHitBox hitbox { get; set; }
+        void Update(double dt);
+        void Draw();
+    }
+}
