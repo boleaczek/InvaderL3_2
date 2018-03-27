cb45319c48c66ea611576804aeb2a15ad365e2b1
boleaczek <bart.oleaczek@gmail.com>
2018-03-27 16:11:22 +0200

    added logic library

---
+++ InvaderLogicLibrary/IHitBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvaderLogicLibrary
+{
+    interface IHitBox
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        int Height { get; set; }
+        int Length { get; set; }
+
+        bool IsHit(IHitBox);
+    }
+}
