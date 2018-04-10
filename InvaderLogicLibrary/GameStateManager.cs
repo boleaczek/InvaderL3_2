d61e0eb163cc9bc8da8d490d564e42df336626bd
Kamil R <kamil.r@interia.pl>
2018-04-10 16:45:40 +0200

    added gamestate

---
+++ InvaderLogicLibrary/GameStateManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvaderLogicLibrary
+{
+    class GameStateManager
+    {
+        IGameState currentState;
+
+        GameStateManager(IGameState defaultState)
+        {
+            this.currentState = defaultState;
+        }
+
+        void SetState(IGameState newState)
+        {
+            this.currentState.OnUnload();
+            this.currentState = newState;
+            this.currentState.OnLoad();
+        }
+
+        void Update(double dt)
+        {
+            this.currentState.OnUpdate(dt);
+        }
+
+        void Draw()
+        {
+            this.currentState.OnDraw();
+        }
+
+
+    }
+
+}
